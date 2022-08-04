f82968cd6d4a8bf77040edd2a560accf49ac9ce7
Long Tran <longtran@192.168.2.21>
2022-08-04 14:23:13 +0700

    take picture by camera

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import './add_place_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({Key? key}) : super(key: key);
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : const Text("No image taken"),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextButton.icon(
+            icon: const Icon(Icons.camera),
+            label: const Text(
+              'Take picture',
+              textAlign: TextAlign.center,
+            ),
+            style: ButtonStyle(
+              textStyle: MaterialStateProperty.all<TextStyle>(
+                TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+            ),
+            onPressed: _takePicture,
+          ),
+        )
+      ],
+    );
+  }
+}
